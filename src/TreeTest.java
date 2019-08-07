5f96eb604ff0745df9dd797414c65832c97b34aa
adria <Konfitury2>
2019-08-07 14:17:29 +0200

    uproszczenie klasy TreeTest

--- src/TreeTest.java
+++ src/TreeTest.java
@@ -6,19 +6,17 @@ public class TreeTest {
         Scanner scanner = new Scanner(System.in);
         Tree[] trees = new Tree[3];
 
-
         for (int i = 0; i < trees.length; i++) {
-            do {
-                System.out.println("Podaj gatunek i wysokość drzewa");
-                Tree tree = new Tree(scanner.nextLine(), scanner.nextDouble());
-                scanner.nextLine();
+            System.out.println("Podaj gatunek i wysokość drzewa");
+            Tree tree = new Tree(scanner.nextLine(), scanner.nextDouble());
+            scanner.nextLine();
 
-                if (isDuplicate(trees, tree)) {
-                    trees[i] = tree;
-                } else {
-                    System.out.println("Duplikat");
-                }
-            } while (trees[i] == null);
+            if (isDuplicate(trees, tree)) {
+                trees[i] = tree;
+            } else {
+                System.out.println("Duplikat");
+                i--;
+            }
         }
         System.out.println(Arrays.toString(trees));
         scanner.close();
